34e6320c87d1de720f45631533136f1febc79c75
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-07-01 23:50:42 -0400

    using filter and printing the data on webpage

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,17 @@
 import React from 'react'
 
 
-const BlogList = ({blogs}) => {
+const BlogList = ({blogs,title}) => {
   // const blogs=props.blogs
   // console.log(props,blogs)
   return (
     <div className='blog-list' >
+      <h2>{title}</h2>
       {blogs.map((blog)=>(
         <div className="blog-preview" key={blog.id} >
-        <h2>{blog.author}</h2>
+        <h2>{blogs.author}</h2>
         <h4>Written by {blog.author}</h4>
-        <p>Mesage {blog.body}</p>
+        <p>Message {blog.body}</p>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import {useState,React } from 'react'
 import BlogList from './BlogList';
 
 const Home=()=>{
-  const [blog,setBlogs]=useState([
+  const [blogs,setBlogs]=useState([
     {title:'How to learn anything',body:'right now is the time you need to start',author:'Amit',id:1},
     {title:'maybe not everything',body:'maybe plan ahead',author:'Kum',id:2},
     {title:'How to learn anything',body:'maybe desctructer the plan',author:'Singh',id:3}
@@ -11,11 +11,9 @@ const Home=()=>{
 
   return (
     <div className='home'>
-      <BlogList  blogs={blog} />
-      
-        
-       
-       
+      <BlogList  blogs={blogs} title="All Blog"  />
+      <BlogList  blogs={blogs.filter((blog=>blog.author==='Amit' ))}title="Amit blog"  />
+
     </div>
   )
 }
