db85cf6e0b5cbd875beb1d7a4df9d3cf650b4cb6
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-07-03 15:22:19 -0400

    using useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import {useState,React } from 'react'
+import {useState,React,useEffect } from 'react'
 import BlogList from './BlogList';
 
 const Home=()=>{
@@ -7,17 +7,27 @@ const Home=()=>{
     {title:'maybe not everything',body:'maybe plan ahead',author:'Kum',id:2},
     {title:'How to learn anything',body:'maybe desctructer the plan',author:'Singh',id:3}
   ]);
+  
+  const [firstName,setfirstName]=useState('Jerremy')
+
   const handleDelete=(id)=>{
     const newBlogs=blogs.filter(blog=>blog.id!=id);
     setBlogs(newBlogs);
 
   }
-  
+  useEffect(()=> {
+    console.log('use effect ran')
+    console.log(firstName)
+
+  },[firstName]);
 
 
   return (
     <div className='home'>
       <BlogList  blogs={blogs} title="All Blog" handleDelete={handleDelete} />
+      <p>{firstName}</p>
+      <button onClick={()=> setfirstName('Tenadu')}>Change Name</button>
+      
       
 
     </div>
