419937b46fc5c44b92161360e87de0611daa84b2
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-07-04 16:02:46 -0400

    handling fetch erros

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home=()=>{
   const [blogs,setBlogs]=useState(null);
   const [isPending,setisPending]=useState(true)
+  const [error,setError]=useState(null);
 
 
   // const handleDelete=(id)=>{
@@ -13,23 +14,31 @@ const Home=()=>{
   // }
   useEffect(()=> {
     setTimeout(()=>{
-      fetch('http://localhost:8000/blogs')
-    .then(res=>{
-      return res.json();
-    })
-    .then(blogs=>{
-      setBlogs(blogs)
-      setisPending(false)
-
-    })
+      fetch('http://localhost:8000/blogss')
+      .then(res=>{
+        if(!res.ok){
+          throw Error('could not fetch the data for that resouce')
+        }
+        return res.json();
+      })
+      .then(blogs=>{
+        setisPending(false);
+        setBlogs(blogs);
+        setError(null);
+      })
+      .catch(err=>{
+        setisPending(false)
+        setError(err.message)
+      })
     },1000) 
     
   },[]);
 
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading.... </div>  }
-      {blogs && <BlogList  blogs={blogs} title="All Blog" />}
+      {blogs && <BlogList  blogs={blogs} />}
 
     </div>
   )
