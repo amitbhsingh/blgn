7729f442b7d609c528f1fe4c20a9037a634b6efa
amitkumar singh <amitkumarsingh@amitkumars-MacBook-Pro.local>
2023-07-07 21:57:10 -0400

    form for creating the blog

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
+
 const Create = () => {
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+        type='text'
+        required
+        />
+        <textarea 
+        required
+        >
+        </textarea>
+      </form>
+      <label>Blog author:</label>
+      <select>
+      <option value='mario'> mario</option>
+      <option value='yoshi'> yoshi</option>
+      </select>
+      <button>Add Blog</button>
     </div>
   )
 }
