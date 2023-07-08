163f364e363e4144c88ae48f8796aa0c1b1562d2
amitkumar singh <amitkumarsingh@amitkumars-MBP.local.tld>
2023-07-08 18:08:49 -0400

    real data printing on website from input forms

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const Create = () => {
+  const [title,setTittle]=useState('');
+  const [body,setBody]=useState('');
+  const [author,setAuthor]=useState('amit');
   return (
     <div className='create'>
       <h2>Add a New Blog</h2>
@@ -10,18 +13,27 @@ const Create = () => {
         <input
         type='text'
         required
+        value={title}
+        onChange={(e)=>setTittle(e.target.value)}
         />
         <textarea 
         required
+        value={body}
+        onChange={(e)=>setBody(e.target.value)}
         >
         </textarea>
       </form>
       <label>Blog author:</label>
-      <select>
+      <select
+      value={author}
+      onChange={(e)=> setAuthor(e.target.value)} >
       <option value='mario'> mario</option>
       <option value='yoshi'> yoshi</option>
       </select>
       <button>Add Blog</button>
+      <p>Title{title}</p>
+      <p>Body{body}</p>
+      <p>Written by {author}</p>
     </div>
   )
 }
